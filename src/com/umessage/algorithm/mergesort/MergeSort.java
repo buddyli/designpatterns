b06c3f5cb6d283c09cf79557fe80356f9b8e4835
= <=>
2014-07-24 11:14:53 +0800

    add merge sort

---
+++ src/com/umessage/algorithm/mergesort/MergeSort.java
@@ -0,0 +1,19 @@
+package com.umessage.algorithm.mergesort;
+
+/**
+ * 归并排序，递归的经典使用。
+ * 
+ * @author licb
+ * 
+ */
+public class MergeSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
