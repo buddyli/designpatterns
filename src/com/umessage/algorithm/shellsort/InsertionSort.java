e0a5d8da3b396c3188f97a52bcd0a704a18d4e32
licb <licb@licb-ThinkPad-Edge.(none)>
2014-08-01 09:38:03 +0800

    add insertion sort and shell sort

---
+++ src/com/umessage/algorithm/shellsort/InsertionSort.java
@@ -0,0 +1,46 @@
+package com.umessage.algorithm.shellsort;
+
+/**
+ * 插入排序，时间复杂度为O(N^2)，如果源数组是预先排序好的，那么时间复杂度为O(N)。
+ * 
+ * 插入排序由N-1趟<strong>(不是N-1次)</strong>排序组成。对于p=1到N-1，插入排序保证从位置0到p上的元素为已排序状态。
+ * 
+ * 插入排序利用了这样的事实：已知位置0到位置p-1上的元素已经处于排过序的状态。
+ * 
+ * @author licb
+ * 
+ */
+public class InsertionSort {
+
+	/**
+	 * 
+	 * @param array
+	 *            要排序的Comparable源数组
+	 */
+	public static <T extends Comparable<? super T>> void insertionSort(T[] array) {
+		int j;
+
+		// 第p趟，将位置p上的元素向左移动，直到在它前p+1个元素中的正确位置被找到的地方(注意：p是从1开始的)。
+		for (int p = 1; p < array.length; p++) {
+
+			// 27-33行实现数据移动而没有明显的试用交换
+			T tmp = array[p];
+
+			for (j = p; j > 0 && tmp.compareTo(array[j - 1]) < 0; j--) {// 如果源数组是有序的，这么这里的子逻辑不会执行，因为第二个compare条件不满足。
+				array[j] = array[j - 1];
+			}
+
+			array[j] = tmp;
+		}
+	}
+
+	public static void main(String[] args) {
+		Integer[] array = { 10, 5, 87, 9, 8 };
+		insertionSort(array);
+
+		for (Integer item : array) {
+			System.out.print(item);
+			System.out.print(", ");
+		}
+	}
+}
---
+++ src/com/umessage/algorithm/shellsort/ShellSort.java
@@ -0,0 +1,49 @@
+package com.umessage.algorithm.shellsort;
+
+/**
+ * 希尔排序，冲破二次时间屏障的第一批算法之一。通过合理的选择增量序列，时间复杂度可以达到O(N^4/3)或者O(N^7/6)。
+ * 
+ * 因为每趟比较所用的距离随着算法的进行而减小，直到只比较相邻元素的最后一趟排序为止。因此有时也叫做缩减增量排序（diminishing increment
+ * sort）。
+ * 
+ * 增量序列的一个流行的选择方式是：N/2的下界，下一个增量为上一个增量的/2下界。
+ * 
+ * @author licb
+ * 
+ */
+public class ShellSort {
+
+	public static <T extends Comparable<? super T>> void shellSort(T[] array) {
+		int j;
+
+		// 选择增量为数组长度的1/2的下界。
+		for (int gap = array.length / 2; gap > 0; gap /= 2) {
+
+			// 从gap开始，对相隔gap个位置的元素进行排序
+			for (int i = gap; i < array.length; i++) {
+				T tmp = array[i];
+
+				for (j = i; j >= gap && tmp.compareTo(array[j - gap]) < 0; j -= gap) {
+					array[j] = array[j - gap];
+				}
+
+
+				array[j] = tmp;
+			}
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Integer[] array = { 10, 5, 87, 9, 8 };
+		shellSort(array);
+
+		for (Integer item : array) {
+			System.out.print(item);
+			System.out.print(", ");
+		}
+	}
+
+}
